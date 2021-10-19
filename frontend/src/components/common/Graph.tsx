b59960f582593934dd4a7d5ab97b761ccd80d33c
Aurelio Arcabascio <aurelioarcabascio@gmail.com>
2021-10-19 04:23:48 -0400

    Fixed indentation on Graph.tsx

--- frontend/src/components/common/Graph.tsx
+++ frontend/src/components/common/Graph.tsx
@@ -26,12 +26,12 @@ const Graph = (): JSX.Element => {
   })
 
     return (
-        <LineChart className="price-graph" width={size?.width / 1.3} height={450} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-            <Line type="linear" dataKey="price" stroke="#8884d8" />
-            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
+      <LineChart className="price-graph" width={size?.width / 1.3} height={450} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+          <Line type="linear" dataKey="price" stroke="#8884d8" />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
       </LineChart>
     )
 }
